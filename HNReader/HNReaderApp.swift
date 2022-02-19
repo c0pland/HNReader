570c13c2ab9c2ee12aae73eba2bfd0abbd067fd2
Bogdan <c0pland@icloud.com>
2022-02-19 21:00:18 +0700

    Initial Commit

---
+++ HNReader/HNReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNReaderApp.swift
+//  HNReader
+//
+//  Created by Богдан Беннер on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HNReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
